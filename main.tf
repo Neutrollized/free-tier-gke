29bd0873721244c9b59ae60c21b45792c2425785
Tom Whitwell <tom@whi.tw>
2023-06-26 11:07:24 +0100

    Refer to cluster by id, not name
    If the cluster is replaced, but the node pool does not 'need to be',
    the node-pool is not flagged for replacement when it should be.
    
    It is deleted within GKE when the cluster itself is removed, but
    terraform's state does not reflect this. When terraform then creates
    the 'new' cluster, no node pool is created as, as far as terraform is
    aware, it already exists.
    
    By referring to the cluster by id which is not known at plan time for
    a creation, the node pool is correctly flagged for replacement.
    
    ref: https://github.com/hashicorp/terraform-provider-google/issues/
       11164#issuecomment-1050189855

--- main.tf
+++ main.tf
@@ -146,7 +146,7 @@ resource "google_container_cluster" "primary" {
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name     = var.gke_nodepool_name
   location = var.regional ? var.region : var.zone
-  cluster  = google_container_cluster.primary.name
+  cluster  = google_container_cluster.primary.id
 
   initial_node_count = var.initial_node_count
   autoscaling {
